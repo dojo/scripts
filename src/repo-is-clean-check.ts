3ab211378e598072543505882034ba2c62188c0c
Anthony Gubler <agubler@sitepen.com>
2018-08-13 15:24:36 +0100

    Add yargs for the repo is clean check

--- src/repo-is-clean-check.ts
+++ src/repo-is-clean-check.ts
@@ -1,7 +1,17 @@
+import * as yargs from 'yargs';
 import { isRepoClean } from './utils/checks';
 
+const {
+	branch: releaseBranch
+}: {
+	branch: string;
+} = yargs.option('branch', {
+	type: 'string',
+	default: 'master'
+}).argv as any;
+
 (async function() {
-	if (!(await isRepoClean())) {
+	if (!(await isRepoClean(releaseBranch))) {
 		process.exit(1);
 	}
 })();
